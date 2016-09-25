93a18ef5ca8880ea10a231feab8157f626fa3e87
MatiRosero <gekidasa@gmail.com>
2016-09-24 18:01:00 -0600

    joints menu done

--- header.php
+++ header.php
@@ -23,63 +23,35 @@
 
 <?php echo file_get_contents( get_template_directory() . '/assets/dist/sprite/sprite.svg' ); ?>
 
-<!-- TOPBAR -->
-<div class="title-bar" data-responsive-toggle="main-menu" data-hide-for="medium">
-  <button class="menu-icon" type="button" data-toggle></button>
-  <div class="title-bar-title"><?php bloginfo( 'name' ); ?></div>
-</div>
+<header id="masthead" class="" role="banner">
+  <section class="expanded row small-collapse">
 
-<div class="top-bar" id="main-menu">
-  <div class="top-bar-left">
-    <ul class="dropdown menu" data-dropdown-menu>
-      <li class="menu-text"><?php bloginfo( 'name' ); ?></li>
-    </ul>
-  </div>
-  <div class="top-bar-right">
-    <?php eco_top_nav(); ?>
-  </div>
-</div>
-<!-- END TOPBAR -->
+    <!-- TOPBAR -->
+    <div class="title-bar" data-responsive-toggle="main-menu" data-hide-for="medium">
+      <button class="menu-icon" type="button" data-toggle></button>
+      <div class="title-bar-title"><?php bloginfo( 'name' ); ?></div>
+    </div>
+
+    <nav id="main-menu" class="top-bar">
+      <section class="top-bar-left">
+        <ul class="dropdown menu" data-dropdown-menu>
+          <!-- <h1 class="site-title"> -->
+          <li class="menu-text">            
+            <a href="<?php esc_attr_e( home_url( '/' ) ); ?>" rel="home">
+              <?php bloginfo( 'name' ); ?>
+            </a></li>
+        </ul>
+      </section>
+      <section class="top-bar-right">
+        <?php eco_top_nav(); ?>
+      </section>
+    </nav><!-- #site-navigation -->
+    <!-- END TOPBAR -->
+
+  </section><!-- expanded row column -->
+</header><!-- #masthead -->
 
 
-<div class="off-canvas-wrapper">
-  <div class="off-canvas-wrapper-inner" data-off-canvas-wrapper>
-    <div class="title-bar show-for-small-only">
-      <div class="title-bar-left">
-        <button class="menu-icon" type="button" data-toggle="offCanvasLeft"></button>
-        <span class="title-bar-title"><?php bloginfo( 'name' ); ?></span>
-      </div>
-    </div><!-- .title-bar -->
-    <div class="off-canvas position-left" id="offCanvasLeft" data-off-canvas>
-			<button class="close-button" aria-label="Close menu" type="button" data-close>
-				<span aria-hidden="true">&times;</span>
-			</button>
-			<?php
-			 $args = array (
-				 'theme_location' 	=> 'primary',
-				 'container' 				=> 'nav',
-				 'container_class'	=> 'offcanvas-navigation',
-				 'menu_class' 			=> 'mobile-menu',
-			 );
-				wp_nav_menu( $args );
-			?>
-    </div><!-- #offCanvasLeft -->
-    <div class="off-canvas-content" data-off-canvas-content>
 
-			<header id="masthead" class="" role="banner">
-				<section class="expanded row column">
-					<h1 class="site-title">
-						<a href="<?php esc_attr_e( home_url( '/' ) ); ?>" rel="home">
-							<?php bloginfo( 'name' ); ?>
-						</a>
-					</h1>
-					<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
-				</section>
-				<nav id="site-navigation" class="top-bar show-for-medium" data-topbar role="navigation">
-					<section class="top-bar-section row column">
-						<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
-					</section>
-				</nav><!-- #site-navigation -->
-			</header><!-- #masthead -->
 
 			<div id="content" class="site-content">
--- includes/menu.php
+++ includes/menu.php
@@ -29,17 +29,17 @@ class Topbar_Menu_Walker extends Walker_Nav_Menu {
 }
 
 // The Off Canvas Menu
-// function joints_off_canvas_nav() {
-// 	 wp_nav_menu(array(
-//         'container' => false,                           // Remove nav container
-//         'menu_class' => 'vertical menu',       // Adding custom nav class
-//         'items_wrap' => '<ul id="%1$s" class="%2$s" data-accordion-menu>%3$s</ul>',
-//         'theme_location' => 'main-nav',        			// Where it's located in the theme
-//         'depth' => 5,                                   // Limit the depth of the nav
-//         'fallback_cb' => false,                         // Fallback function (see below)
-//         'walker' => new Off_Canvas_Menu_Walker()
-//     ));
-// } 
+function eco_off_canvas_nav() {
+	 wp_nav_menu(array(
+        'container' => false,                           // Remove nav container
+        'menu_class' => 'vertical menu',       // Adding custom nav class
+        'items_wrap' => '<ul id="%1$s" class="%2$s" data-accordion-menu>%3$s</ul>',
+        'theme_location' => 'primary',        			// Where it's located in the theme
+        'depth' => 5,                                   // Limit the depth of the nav
+        'fallback_cb' => false,                         // Fallback function (see below)
+        'walker' => new Off_Canvas_Menu_Walker()
+    ));
+} 
 
 class Off_Canvas_Menu_Walker extends Walker_Nav_Menu {
     function start_lvl(&$output, $depth = 0, $args = Array() ) {
