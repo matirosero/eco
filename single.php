ca7f94b42b08939e7abc7f17bef6244d34bb2413
MatiRosero <gekidasa@gmail.com>
2016-10-17 17:08:05 -0600

    single blog post everything but comments

--- single.php
+++ single.php
@@ -15,14 +15,22 @@ get_header(); ?>
 
 	<div class="medium-8 large-9 xxlarge-10 columns">
 
-		<div id="primary" class="content-area row" data-equalizer-watch > 
+		<div id="primary" class="content-area row" data-equalizer-watch >
 			<main id="main" class="site-main large-centered large-10 xxlarge-8 columns" role="main">
 
 			<?php while ( have_posts() ) : the_post(); ?>
 
 				<?php get_template_part( 'components/content', 'single' ); ?>
 
-				<?php the_post_navigation(); ?>
+				<?php the_post_navigation(
+					array(
+			            'prev_text'                  => __( '<i class="fa fa-arrow-left" aria-hidden="true"></i> %title' ),
+			            'next_text'                  => __( '%title <i class="fa fa-arrow-right" aria-hidden="true"></i>' ),
+			            // 'in_same_term'               => true,
+			            // 'taxonomy'                   => __( 'post_tag' ),
+			            // 'screen_reader_text' => __( 'Continue Reading' ),
+        			)
+        		); ?>
 
 				<?php
 					// If comments are open or we have at least one comment, load up the comment template.
