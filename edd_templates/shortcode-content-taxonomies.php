3c3db4d9ffae0cb0e0ad98020c6018a0459f6fb3
MatiRosero <gekidasa@gmail.com>
2016-10-19 17:41:29 -0600

    hid tags from taxonomies shortcode, so that we can sort free stuff without tag showing up

--- edd_templates/shortcode-content-taxonomies.php
+++ edd_templates/shortcode-content-taxonomies.php
@@ -10,21 +10,23 @@ $download_categories = get_the_term_list( // get the download categories
 	', ',
 	''
 );
-$download_tags = get_the_term_list( // get the download tags
-	$post->ID,
-	'download_tag',
-	'<span class="download-categories-title"></span>',
-	', ',
-	''
-);
+// $download_tags = get_the_term_list( // get the download tags
+// 	$post->ID,
+// 	'download_tag',
+// 	'<span class="download-categories-title"></span>',
+// 	', ',
+// 	''
+// );
 
 if ( $download_categories || $download_tags ) {
 	?>
 	<div class="shop-download-terms">
 		<?php echo $download_categories ?>
 	</div>
+	<?php /*
 	<div class="shop-download-terms">
 		<?php echo $download_tags ?>
 	</div>
+	*/ ?>
 	<?php
 }
